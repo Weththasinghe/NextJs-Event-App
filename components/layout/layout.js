d86097c59ce6c3c794cb05a54323df35caa0aeab
Weththasinghe <thathsara48@gmail.com>
2022-12-30 10:24:11 +0530

    finish commit

--- components/layout/layout.js
+++ components/layout/layout.js
@@ -1,11 +1,25 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useContext } from "react";
+import NotificationContext from "../../store/notification-context";
+import Notification from "../ui/notification";
 import MainHeader from "./main-header";
 
+
 function Layout(props) {
+  const notificationCtx = useContext(NotificationContext);
+
+  const activeNotification =notificationCtx.notification;
+
   return (
     <Fragment>
         <MainHeader /> 
-      <main>{props.children}</main>
+        <main>{props.children}</main>
+      {activeNotification && (
+      <Notification
+        title={activeNotification.title} 
+        message={activeNotification.message} 
+        status={activeNotification.status}
+      />
+    )}
     </Fragment>
   );
 }
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,21 @@
 import Head from 'next/head';
-
+import Notification from '../components/ui/notification';
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 
 import Layout from "../components/layout/layout";
+import { Component } from 'react';
+import { NotificationContextProvider } from '../store/notification-context';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
+    <NotificationContextProvider>
     <Layout>
       <Head>
         <meta name='viewport' content='initial-scale=1.0, width=device-width' />
       </Head>
       <Component {...pageProps} />
     </Layout>
+    </NotificationContextProvider>
   );
 }
